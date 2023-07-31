cf009f8b5a7d0ed920afdb87f68e928aff7baa4b
jeremykc <jeremyk.c._101@hotmail.com>
2023-07-31 02:36:18 -0400

    Add 'Country Geotargeting' proxy settings

--- carpart_weight_scraper/carpart_weight_scraper/settings.py
+++ carpart_weight_scraper/carpart_weight_scraper/settings.py
@@ -43,6 +43,9 @@ SCRAPEOPS_FAKE_HEADERS_ENABLED = True
 # Enable The ScrapeOps Proxy Aggregator
 SCRAPEOPS_PROXY_ENABLED = True
 
+# ScrapeOps Proxy Settings
+SCRAPEOPS_PROXY_SETTINGS = {'country': 'us'}
+
 # Maximum concurrent requests for ScrapeOps Proxy
 CONCURRENT_REQUESTS = 1     # Free Plan: 1, 250K Credits: 5, 500K Credits: 10
 
